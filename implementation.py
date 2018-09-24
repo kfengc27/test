0fcf511466b03e69df5563eadf1a958643acc116
Kevin Feng <kfengc27@gmail.com>
2018-09-24 22:23:55 +1000

    Add files via upload

--- implementation.py
+++ implementation.py
@@ -59,11 +59,44 @@ def preprocess(review):
 
 #Reference: https://github.com/tensorflow/tensorflow/issues/16186
 def lstm_cell():
-   lstm = tf.contrib.rnn.BasicLSTMCell(BATCH_SIZE, forget_bias=1.0, state_is_tuple=True, dtype=tf.float32)
-   lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=tf.placeholder_with_default(0.75, shape=[], name='dropout_keep_prob')) 
-   return lstm
+    lstm = tf.contrib.rnn.BasicLSTMCell(BATCH_SIZE, forget_bias=1.0, state_is_tuple=True, dtype=tf.float32)
+#    lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=tf.placeholder_with_default(0.75, shape=[], name='dropout_keep_prob')) 
+    return lstm
 
-#Version 2 
+
+def define_graph():
+
+    input_data= tf.placeholder(dtype=tf.float32,shape=[BATCH_SIZE,MAX_WORDS_IN_REVIEW, EMBEDDING_SIZE], name='input_data')
+    labels = tf.placeholder(dtype=tf.float32,shape=[BATCH_SIZE,2], name='labels')
+    dropout_keep_prob = tf.placeholder_with_default(1.0, shape=[], name='dropout_keep_prob')
+    # lstm = tf.nn.rnn_cell.BasicLSTMCell(64, forget_bias=1.0, state_is_tuple=True, dtype=tf.float32)
+    # lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=dropout_keep_prob)
+    l_cell = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(2)]) #Double lstm
+    init = l_cell.zero_state(BATCH_SIZE, dtype=tf.float32)
+    drop_first = tf.contrib.rnn.DropoutWrapper(l_cell, output_keep_prob=dropout_keep_prob)
+    outputs, states= tf.nn.dynamic_rnn(drop_first, input_data, initial_state=init)
+    # weight = tf.Variable(tf.truncated_normal([BATCH_SIZE, 2]))
+    # bias = tf.Variable(tf.constant(0.1, shape=[2]))
+    # results = tf.layers.dense(states[1], 2, activation=tf.nn.sigmoid)
+    # results = tf.layers.dense(states[1], 1, tf.nn.relu)
+    # results = tf.matmul(states[1], weight) + bias
+    # batch_range = tf.range(tf.shape(outputs)[0])
+    # indices = tf.stack([batch_range, BATCH_SIZE-1], axis=1)
+    # res = tf.gather_nd(outputs, indices)
+    # outputs = tf.unstack(tf.transpose(outputs, [1,0,2]))
+    # logits= tf.layers.dense(inputs=states[1], units=2, activation=tf.nn.sigmoid)
+    # logits= tf.layers.dense(inputs=states[1], units=2, activation=tf.nn.sigmoid)
+    dense = tf.layers.dense(outputs[:,-1], 50, activation=tf.nn.relu)
+    drop_second = tf.layers.dropout(dense, rate=(1-dropout_keep_prob))
+    logits = tf.layers.dense(drop_second, 2, activation=None)
+    loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels=labels), name='loss')
+    optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
+    pred  = tf.nn.sigmoid(logits)
+    correct_prediction = tf.equal(tf.argmax(pred,1),tf.argmax(labels,1))
+    accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32), name='accuracy')
+    return input_data, labels, dropout_keep_prob, optimizer, accuracy, loss
+
+# VERSION 3.0
 # def define_graph():
 
 #     input_data= tf.placeholder(dtype=tf.float32,shape=[BATCH_SIZE,MAX_WORDS_IN_REVIEW, EMBEDDING_SIZE], name='input_data')
@@ -72,7 +105,6 @@ def lstm_cell():
 
 #     lstm = tf.nn.rnn_cell.BasicLSTMCell(64, forget_bias=1.0, state_is_tuple=True, dtype=tf.float32)
 #     lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=dropout_keep_prob)
-#     # l_cell = tf.contrib.rnn.MultiRNNCell([lstm_cell() for _ in range(2)]) #Double lstm
     
 #     init = lstm.zero_state(BATCH_SIZE, dtype=tf.float32)
 #     outputs, states= tf.nn.dynamic_rnn(lstm, input_data, initial_state=init)
@@ -88,43 +120,11 @@ def lstm_cell():
 #     logits= tf.layers.dense(inputs=states.h, units=2, activation=None)
 
 #     loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels=labels), name='loss')
-#     optimizer = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
-
+#     optimizer = tf.train.AdamOptimizer().minimize(loss)
 #     correct_prediction = tf.equal(tf.argmax(logits,1),tf.argmax(labels,1))
 #     accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32), name='accuracy')
 
 #     return input_data, labels, dropout_keep_prob, optimizer, accuracy, loss
-
-#VERSION 3.0
-def define_graph():
-
-    input_data= tf.placeholder(dtype=tf.float32,shape=[BATCH_SIZE,MAX_WORDS_IN_REVIEW, EMBEDDING_SIZE], name='input_data')
-    labels = tf.placeholder(dtype=tf.float32,shape=[BATCH_SIZE,2], name='labels')
-    dropout_keep_prob = tf.placeholder_with_default(1.0, shape=[], name='dropout_keep_prob')
-
-    lstm = tf.nn.rnn_cell.BasicLSTMCell(64, forget_bias=1.0, state_is_tuple=True, dtype=tf.float32)
-    lstm = tf.contrib.rnn.DropoutWrapper(cell=lstm, output_keep_prob=dropout_keep_prob)
-    
-    init = lstm.zero_state(BATCH_SIZE, dtype=tf.float32)
-    outputs, states= tf.nn.dynamic_rnn(lstm, input_data, initial_state=init)
-    # weight = tf.Variable(tf.truncated_normal([BATCH_SIZE, 2]))
-    # bias = tf.Variable(tf.constant(0.1, shape=[2]))
-    # results = tf.layers.dense(states[1], 2, activation=tf.nn.sigmoid)
-    # results = tf.layers.dense(states[1], 1, tf.nn.relu)
-    # results = tf.matmul(states[1], weight) + bias
-    # batch_range = tf.range(tf.shape(outputs)[0])
-    # indices = tf.stack([batch_range, BATCH_SIZE-1], axis=1)
-    # res = tf.gather_nd(outputs, indices)
-    # outputs = tf.unstack(tf.transpose(outputs, [1,0,2]))
-
-    logits= tf.layers.dense(inputs=states.h, units=2, activation=None)
-
-    loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits,labels=labels), name='loss')
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
-    correct_prediction = tf.equal(tf.argmax(logits,1),tf.argmax(labels,1))
-    accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32), name='accuracy')
-
-    return input_data, labels, dropout_keep_prob, optimizer, accuracy, loss
 
 # VERSION 1.0  
 # def define_graph():
@@ -156,18 +156,3 @@ def define_graph():
 #     accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32),name="accuracy")
 #     return input_data, labels, dropout_keep_prob, optimizer, accuracy, loss
 
-# # def lstm_cell():
-# #     return tf.contrib.rnn.BasicLSTMCell(150)
-
-# # def define_graph():
-# #     """
-# #     Implement your model here. You will need to define placeholders, for the input and labels,
-# #     Note that the input is not strings of words, but the strings after the embedding lookup
-# #     has been applied (i.e. arrays of floats).
-# #     In all cases this code will be called by an unaltered runner.py. You should read this
-# #     file and ensure your code here is compatible.
-# #     Consult the assignment specification for details of which parts of the TF API are
-# #     permitted for use in this function.
-# #     You must return, in the following order, the placeholders/tensors for;
-# #     RETURNS: input, labels, optimizer, accuracy and loss
-# #     """
